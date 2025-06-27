6a1c36216ff4763dfdd54cda863a6d355cf06b22
Abderrahim Laribi <abderrahimlaribiesi@gmail.com>
2025-06-27 12:27:33 +0100

    feat: routes defined

---
+++ app/companion/new/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const newCompanion = () => {
+    return (
+        <div>newCompanion</div>
+    )
+}
+export default newCompanion
---
+++ app/companion/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>
+            <h1 className="walker">
+                This is the Companion Page
+            </h1>
+        </div>
+    )
+}
+export default Page
